aa66ce4df345c76bdcbe11112dddcdef157fe7e2
shizukanewbot <pinka25u7@gmail.com>
2025-06-08 09:19:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,7 +35,7 @@ API_KEY = getenv("API_KEY", None) # youtube song api key, generate free key or b
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/naynamusicbot/Aviax_nayana",
+    "https://github.com/shizukanewbot/Aviax_newahizuka",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
