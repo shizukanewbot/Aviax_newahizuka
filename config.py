b1ec87b835068dc2b8b01eea7ecd73035839d473
naynamusicbot <sachin76u7@gmail.com>
2025-06-08 08:29:37 +0530

    Update config.py

--- config.py
+++ config.py
@@ -35,15 +35,15 @@ API_KEY = getenv("API_KEY", None) # youtube song api key, generate free key or b
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/A31MUSICBOT/Aviax_a31music",
+    "https://github.com/naynamusicbot/Aviax_nayana",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
     "GIT_TOKEN", None
 )  # Fill this variable if your upstream repository is private
 
-SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/a31MAAX")
-SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/NIGHTMAAREE_43")
+SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/MUSIC_BOT_UPDATE")
+SUPPORT_GROUP = getenv("SUPPORT_GROUP", "https://t.me/MUSIC_BOT_UPDATE")
 
 # Set this to True if you want the assistant to automatically leave chats after an interval
 AUTO_LEAVING_ASSISTANT = bool(getenv("AUTO_LEAVING_ASSISTANT", False))
